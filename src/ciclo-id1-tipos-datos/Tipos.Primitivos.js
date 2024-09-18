db6772b0489fd6f5aac886710d4d1c31b17b8285
Luis Mac <luismacod5@gmail.com>
2024-09-17 21:02:37 -0600

    feat: Add configuration files and initial code
    - Added .prettierrc file with formatting options
    - Added tsconfig.build.json file for building the project
    - Created app.service.ts with a basic service implementation
    - Added nest-cli.json file with project configuration
    - Created jest-e2e.json file for end-to-end testing
    - Added main.ts file for bootstrapping the application
    - Created app.module.ts with basic module configuration
    - Created app.controller.ts with a basic controller implementation
    - Added Tipos.Primitivos.js and tipos.primitivos.ts files for primitive types example
    - Added tsconfig.json file with compiler options
    - Created app.e2e-spec.ts for end-to-end testing of the root endpoint
    - Created app.controller.spec.ts for unit testing of the AppController
    - Added .eslintrc.js file for ESLint configuration
    - Added .gitignore file to ignore certain files and directories
    - Updated package.json with dependencies and scripts
    - Updated jest configuration in package.json for testing

---
+++ src/ciclo-id1-tipos-datos/Tipos.Primitivos.js
@@ -0,0 +1,22 @@
+// Tipos primitivos
+var nombre = 'Juan';
+var apellidoPaterno = 'Pérez';
+var apellidoMaterno = 'Gómez';
+var edad = 25;
+var casado = false;
+var fechaNacimiento = new Date();
+console.log(
+  'El usuario se llama ' +
+    nombre +
+    ' ' +
+    apellidoPaterno +
+    ' ' +
+    apellidoMaterno +
+    ' y tiene ' +
+    edad +
+    ' años.' +
+    ' ¿Está casado? ' +
+    casado +
+    ' y nació el ' +
+    fechaNacimiento,
+);
---
+++ src/ciclo-id1-tipos-datos/tipos.primitivos.ts
@@ -0,0 +1,23 @@
+// Tipos primitivos
+let nombre: string = 'Juan';
+let apellidoPaterno: string = 'Pérez';
+let apellidoMaterno: string = 'Gómez';
+let edad: number = 25;
+let casado: boolean = false;
+let fechaNacimiento: Date = new Date();
+
+console.log(
+  'El usuario se llama ' +
+  nombre +
+  ' ' +
+  apellidoPaterno +
+  ' ' +
+  apellidoMaterno +
+  ' y tiene ' +
+  edad +
+  ' años.' +
+  ' ¿Está casado? ' +
+  casado +
+  ' y nació el ' +
+  fechaNacimiento,
+);
